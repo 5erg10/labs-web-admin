b6a0fcecb3eb8f417d58d4407ab1e62175af8355
5erg1kal <santamaria.fajardo@gmail.com>
2022-08-07 18:24:53 +0200

    fa path test

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="d-flex justify-content-between align-items-center">
 		<div class="col-auto">
-			<img class="d-inline-block" height="50" src="/static/img/logo.png"/>
+			<img class="d-inline-block" height="50" src="static/img/logo.png"/>
 		</div>
 		<div class="col-auto">
 			<div class="container-fluid">
