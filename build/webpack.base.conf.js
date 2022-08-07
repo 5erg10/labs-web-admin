85afdab43d1b4f92fdaf61eca0089dfd5014702e
5erg1kal <santamaria.fajardo@gmail.com>
2022-08-07 16:45:33 +0200

    add deploy command to npm

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -43,11 +43,7 @@ module.exports = {
       },
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-        loader: 'url-loader',
-        options: {
-          limit: 10000,
-          name: utils.assetsPath('img/[name].[hash:7].[ext]')
-        }
+        loader: 'url-loader'
       },
       {
         test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
@@ -59,11 +55,7 @@ module.exports = {
       },
       {
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-        loader: 'url-loader',
-        options: {
-          limit: 10000,
-          name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
-        }
+        loader: 'url-loader'
       }
     ]
   },
